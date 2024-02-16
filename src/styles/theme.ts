5a49fea328cf79693b4335a539882a01c4ac83fc
Seikeleton <160220611+Seikeleton@users.noreply.github.com>
2024-02-16 18:22:53 +0700

    Add files via upload

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const color = {
+    // primary: "#F0E4CF",
+    primary: "#01c1a6",
+    secondary: "#0E1921",
+    secondaryLight: "#1A2B37",
+    black: "#0A0A0B",
+    white: "#F8F7FF",
+    whiteShade: "#A8ABB4",
+    // bg:"#0A141B",
+    // bg:"#8a8a8a",
+    bg:"#495E57",
+    dinosecondary: "#45474B",
+    dinoprimary: "#01857b",
+    dinohoverbtn: "#01c1a6"
+}
